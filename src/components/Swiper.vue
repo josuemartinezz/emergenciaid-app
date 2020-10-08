53b3f3105730b358ac05405edbbd7546f332d2d0
josue.ayala <ayalavalencia11@gmail.com>
2020-10-07 19:46:38 -0600

    Escaneo de codigo QR

---
+++ src/components/Swiper.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-content">
+      <div class="img-container">
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/564142efe4b09d10c39f8a8f/1594926536902-L2RPB0G2W5J7PK3GSJX8/ke17ZwdGBToddI8pDm48kKSl30YZjGm1cuQENGkIgpd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmQTQPF36sacLINHj8EM-mOpV6TiNljwpig9fIEXFxRtq20zARXe9frESMRzovEkMi/no-person-profile-pic.png"
+          alt=""
+        />
+      </div>
+      <div class="user-information">
+        <div class="detail">
+          <b class="broke">{{ nombre }}</b>
+          <small class="broke">{{ email }} años</small>
+          <small>Dui: {{ dui }}</small>
+          <small>{{ direccion }}</small>
+          <small>{{ ciudad }}</small>
+          <router-link :to="'/view/'+uid">Ver Perfil</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Swiper",
+  props: {
+    uid: {
+      type: String,
+      default: 0,
+    },
+    nombre: {
+      type: String,
+      default: "---",
+    },
+    email: {
+      type: String,
+      default: "---",
+    },
+    dui: {
+      type: String,
+      default: "---",
+    },
+    ciudad: {
+      type: String,
+      default: "---",
+    },
+    direccion: {
+      type: String,
+      default: "---",
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.detail {
+  display: flex;
+  flex-direction: column;
+}
+.broke{
+width: 70%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+b {
+  
+}
+.swiper-container {
+  .swiper-content {
+    width: 100%;
+    display: flex;
+    .img-container {
+      width: 50%;
+      img {
+        width: 100%;
+        padding-left: 45px;
+        padding-top: 50px;
+        padding-bottom: 50px;
+        padding-right: 10px;
+        object-fit: cover;
+      }
+    }
+    .user-information {
+      text-align: start;
+      display: flex;
+      align-items: center;
+      width: 50%;
+    }
+  }
+}
+</style>
--- src/views/QrLector.vue
+++ src/views/QrLector.vue
@@ -1,15 +1,73 @@
 <template>
   <div class="code-qr">
-    <p>
-      Mi codigo es: <b>{{ retorno }}</b>
-    </p>
-    <div style="width: 100%">
+    <div v-if="loading" class="loading"><p>Cargando...</p></div>
+    <div v-if="isShow" class="loading">
+      <b-button @click="isShow = false, openSwiper()" variant="light">Cerrar</b-button>
+    </div>
+    <transition name="slide-fade">
+      <Swiper
+        :nombre="
+          informationUser[0].nombres + ' ' + informationUser[0].apellidos
+        "
+        :dui="informationUser[0].documento_identidad"
+        :ciudad="informationUser[0].nombre"
+        :direccion="informationUser[0].direccion"
+        :email="informationUser[0].email"
+        :uid="informationUser[0].uid"
+        class="swiper"
+        v-if="show"
+      />
+    </transition>
+    <div style="width: 90%; margin: auto">
       <qrcode-stream @detect="onDetect"></qrcode-stream>
     </div>
   </div>
 </template>
 
 <style lang="less">
+.loading {
+  position: fixed;
+  z-index: 500;
+  color: white;
+  background-color: rgba(15, 17, 26, 0.7);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.25s ease;
+  button{
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin: 10px;
+  }
+}
+.slide-fade-enter-active {
+  transition: all 0.7s ease;
+}
+.slide-fade-leave-active {
+  transition: all 2s ease;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateY(2000px);
+  opacity: 0;
+}
+.swiper {
+  border-radius: 10px 10px 0px 0px;
+  position: fixed;
+  background-color: #f2f5fa;
+  z-index: 1000;
+  width: 100%;
+  transition: all 0.25 ease;
+  bottom: 0;
+  border-bottom: solid 5px rgba(45, 143, 235);
+  &.active {
+    bottom: -100 !important;
+  }
+}
+
 .code-qr {
   display: flex;
   justify-content: center;
@@ -20,16 +78,45 @@
 </style>
 
 <script>
+import Swiper from "@/components/Swiper.vue";
+import axios from "axios";
 export default {
+  components: {
+    Swiper,
+  },
   data() {
     return {
       retorno: "",
+      show: false,
+      loading: false,
+      informationUser: {},
+      isShow: false
     };
   },
   methods: {
+    getInfo() {
+      this.loading = true;
+      axios
+        .get(
+          process.env.VUE_APP_BASE_URL +
+            "api/app/perfil.php?action=searchProfileWithUID&uid=" +
+            this.retorno
+        )
+        .then((response) => {
+          this.informationUser = response.data;
+          this.loading = false;
+          this.openSwiper();
+          this.isShow = true
+        });
+    },
+    openSwiper() {
+      this.show = true != this.show;
+    },
     async onDetect(promise) {
       const { imageData, content, location } = await promise;
       this.retorno = content;
+      //this.openSwiper();
+      this.getInfo();
     },
   },
 };
